bc401c2a064a26afa68aca9c5464b33584292944
Yoshie Fujiwara <yoshie.fujiwara@gmail.com>
2021-01-18 22:31:39 -0600

    updated server and db connection

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const { db } = require('./models');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded);
+
+app.get('/', (req, res) => res.send('Hello! wikistack2!!!👋🏻'));
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}👂`);
+});
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
--- server.js
+++ server.js
@@ -1,10 +1,1 @@
 const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello! wikistack2!!!👋🏻'));
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`App listening in port ${port}`);
-});
