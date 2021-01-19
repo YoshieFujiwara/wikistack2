5aff81dd9d11d2fe3bee01017c85a02e2d185007
Yoshie Fujiwara <yoshie.fujiwara@gmail.com>
2021-01-18 21:22:06 -0600

    first stage

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded);
